194391d43055a520dcbd5878947cf6e2456b6924
my_mac_dimitree54 <dimitree54@gmail.com>
2023-02-05 09:52:10 +0200

    firebase setup and configs

--- iosBUBOC/iosBUBOC/iosBUBOC.swift
+++ iosBUBOC/iosBUBOC/iosBUBOC.swift
@@ -1,11 +1,13 @@
 import UIKit
 import sharedBUBOC
+import FirebaseCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         window = UIWindow(frame: UIScreen.main.bounds)
         let mainViewController = Main_iosKt.MainViewController()
         window?.rootViewController = mainViewController
