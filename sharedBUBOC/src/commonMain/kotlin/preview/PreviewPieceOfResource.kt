4ddaa1e8c27cc52840c4389c9e4e8aae935f8361
my_mac_dimitree54 <dimitree54@gmail.com>
2023-02-08 22:33:40 +0200

    instructions now data class too

--- sharedBUBOC/src/commonMain/kotlin/preview/PreviewPieceOfResource.kt
+++ sharedBUBOC/src/commonMain/kotlin/preview/PreviewPieceOfResource.kt
@@ -2,11 +2,11 @@ package preview
 
 import RECIPE_INPUT_ICON
 import androidx.compose.runtime.Composable
-import cuboc.ingredient.PieceOfResource
+import cuboc.ingredient.PieceOfUserResource
 
 
 @Composable
-internal fun PreviewPieceOfResource(pieceOfResource: PieceOfResource) {
+internal fun PreviewPieceOfResource(pieceOfResource: PieceOfUserResource) {
     PreviewCard(
         pieceOfResource.resource.resource.ingredient.name.toString(),
         pieceOfResource.amount.toString() + " " + pieceOfResource.resource.resource.ingredient.measureUnit,
